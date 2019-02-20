471e1a678b754f93487a3f4601ed03b765206e02
flrnprz <rauperez@gmail.com>
2019-02-20 14:36:56 +0100

    update: fisher pkgmanager missing

--- fish/functions/__sf_section_git_status.fish
+++ fish/functions/__sf_section_git_status.fish
@@ -27,6 +27,8 @@ function __sf_section_git_status -d "Display the current git status"
 	# Section
 	# ------------------------------------------------------------------------------
 
+	[ $SPACEFISH_GIT_STATUS_SHOW = false ]; and return
+
 	set -l git_status
 	set -l is_ahead
 	set -l is_behind
@@ -51,7 +53,7 @@ function __sf_section_git_status -d "Display the current git status"
 			set git_status deleted $git_status
 		end
 		if test (string match '*U*' $i)
-			set git_status deleted $git_status
+			set git_status unmerged $git_status
 		end
 	end
 
