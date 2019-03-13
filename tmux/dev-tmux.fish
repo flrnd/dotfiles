5740eddeddbd48871d12e991b66775d0f504dad8
flrnprz <rauperez@gmail.com>
2019-03-13 19:55:37 +0100

    pepperonni

--- tmux/dev-tmux.fish
+++ tmux/dev-tmux.fish
@@ -1,6 +1,6 @@
 function dev-tmux
   tmux new-session -d nvim
-  tmux split-window -h -p 35
+  tmux split-window -h -p 40
   tmux select-pane -t 1
   tmux split-window -v -p 50
   tmux -2 attach-session -d
