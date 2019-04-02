eabca8b33463a9dc05161280263b2f0aacfa83fa
flrnprz <rauperez@gmail.com>
2019-04-02 16:33:26 +0200

    :+1: update

--- fish/aliases.fish
+++
@@ -1,5 +0,0 @@
-# Shortcuts
-alias g="git"
-alias gi="git"
-alias push="git push -u origin master"
-alias nv="nvim"
--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,26 @@
-source ~/.config/fish/aliases.fish
-set SPACEFISH_GIT_SYMBOL ""
+set -x EDITOR vim
+set -x VISUAL $EDITOR
+set -x GIT_EDITOR $EDITOR
+set -x PAGER "less"
+set -x FZF_DEFAULT_COMMAND "fd --type f --follow --hidden --exclude .git --exclude node_modules"
+
+# go support
+if test -e $HOME/go/bin
+  set -x PATH $PATH $HOME/go/bin
+end
+
 if test -d ~/.config/node/global/bin
   set -gx PATH ~/.config/node/global/bin $PATH
 end
 
+# Abbreviations
+abbr nv "nvim"
+abbr ga "git add"
+abbr gcb "git checkout -b"
+abbr gp "git push"
+abbr gpoh "git push -u origin HEAD"
+abbr gs "git status -s"
+
+#prompt
+source ~/.config/fish/functions/fish_prompt.fish
+source ~/.config/fish/functions/rprompt.fish
