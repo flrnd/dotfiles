eb26ff135bd86a166a151fb287a2ebc5f1a4101b
Flrn Prz <rauperez@gmail.com>
2019-07-09 12:57:52 +0200

    git

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -7,7 +7,7 @@ set gray (set_color -o black)
 
 # Fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'no'
+set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showupstream 'yes'
 # set __fish_git_prompt_show_informative_status 'yes'
