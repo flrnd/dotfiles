a85f97ed9265b0f1b2fc59e40d596a965c275c0f
Flrn Prz <rauperez@gmail.com>
2019-07-09 15:22:56 +0200

    prompt colors

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -11,16 +11,16 @@ set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showupstream 'yes'
 # set __fish_git_prompt_show_informative_status 'yes'
-set __fish_git_prompt_color_branch -d magenta
-set __fish_git_prompt_color_dirtystate white
+set __fish_git_prompt_color_branch magenta
+set __fish_git_prompt_color_dirtystate red
 set __fish_git_prompt_color_invalidstate red
 set __fish_git_prompt_color_merging yellow
-set __fish_git_prompt_color_stagedstate yellow
+set __fish_git_prompt_color_stagedstate green
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 # Status Chars
 set __fish_git_prompt_char_dirtystate '*'
-set __fish_git_prompt_char_stagedstate '>'
+set __fish_git_prompt_char_stagedstate '+'
 set __fish_git_prompt_char_untrackedfiles '?'
 set __fish_git_prompt_char_stashstate '<'
 set __fish_git_prompt_char_upstream_ahead '⇡'
@@ -33,7 +33,8 @@ set fish_prompt_pwd_dir_length 0
 function fish_prompt
   set last_status $status
 
-  set_color $fish_color_cwd
+  #set_color $fish_color_cwd
+  set_color blue
   printf '%s' (prompt_pwd)
   set_color normal
 
