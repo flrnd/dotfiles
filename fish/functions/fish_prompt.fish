615aff9d0ac17b7a67d8ac0fee6d7b021a43e259
Flrn Prz <rauperez@gmail.com>
2019-07-09 13:00:31 +0200

    git prompt colors

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -11,10 +11,13 @@ set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showupstream 'yes'
 # set __fish_git_prompt_show_informative_status 'yes'
-set __fish_git_prompt_color_branch -d yellow
+set __fish_git_prompt_color_branch -d magenta
+set __fish_git_prompt_color_dirtystate white
+set __fish_git_prompt_color_invalidstate red
+set __fish_git_prompt_color_merging yellow
+set __fish_git_prompt_color_stagedstate yellow
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
-
 # Status Chars
 set __fish_git_prompt_char_dirtystate '*'
 set __fish_git_prompt_char_stagedstate '>'
