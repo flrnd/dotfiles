996f687a1dc3cc27a4223833ad4e52daecc9f4d7
Florian Rand <florian.rand@gmail.com>
2019-11-01 21:17:27 +0100

    Custom prompt

---
+++ zsh/prompt/prompt.zsh
@@ -0,0 +1,4 @@
+PROMPT='%14F%$((-GITSTATUS_PROMPT_LEN-1))<…<%~%<<%f'   # light blue current working directory
+PROMPT+='${GITSTATUS_PROMPT:+ $GITSTATUS_PROMPT}'      # git status
+PROMPT+=$'\n'                                          # new line
+PROMPT+='%F{%(?.5.196)}❯%f '
