deee9e1d08ab402a3006ed8a7b50359ef6978ead
Florian Rand <florian.rand@gmail.com>
2019-11-01 17:06:36 +0100

    Update function

--- fish/functions/bench_shell.fish
+++ fish/functions/bench_shell.fish
@@ -1,6 +1,14 @@
 function bench_shell
+  set -l time ''
+  switch (uname)
+    case Linux
+      set time '/usr/bin/time'
+    case Darwing
+      set time 'gtime'
+  end
+
   for i in (seq $argv[2])
-    /usr/bin/time -f "\t%E real, %U user, %S sys, %P CPU" $argv[1] -i -c exit
+    $time -f "\t%E real, %U user, %S sys, %P CPU" $argv[1] -i -c exit
   end
 end
 
