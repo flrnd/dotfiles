1a66e579faac3bd9e625d6d6bd7e8617264144fa
Florian Rand <florian.rand@gmail.com>
2019-11-01 19:18:20 +0100

    New zsh setup

---
+++ zsh/alias.zsh
@@ -0,0 +1,31 @@
+# GNU and BSD (macOS) ls flags aren't compatible
+ls --version &>/dev/null
+if [ $? -eq 0 ]; then
+  lsflags="--color --group-directories-first -Fh"
+else
+  lsflags="-GF"
+  export CLICOLOR=1
+fi
+
+# ls
+alias ls="ls ${lsflags}"
+alias la="ls -a"
+alias ll="ls -al"
+alias lr="ls -lR"
+alias lt="ls -ltr"
+alias cat="bat"
+alias fd="fdfind"
+
+# directories
+alias cp="cp -i"
+alias mv="mv -i"
+alias rm="rm -iv"
+alias rmf="rm -ivrf"
+alias mkdir="mkdir -p"
+alias less="less -R"
+
+# mpv filters
+alias mpv-drc='mpv --af="acompressor=ratio=4,loudnorm"'
+alias mpv-nm='mpv --af=lavfi=[pan="stereo|FL=FC+0.30*FL+0.30*FLC+0.30*BL+0.30*SL+0.60*LFE|FR=FC+0.30*FR+0.30*FRC+0.30*BR+0.30*SR+0.60*LFE"]'
+alias mpv-nm-drc='mpv --af=lavfi=[pan="stereo|FL=FC+0.30*FL+0.30*FLC+0.30*BL+0.30*SL+0.60*LFE|FR=FC+0.30*FR+0.30*FRC+0.30*BR+0.30*SR+0.60*LFE,loudnorm"]'
+
---
+++ zsh/config.zsh
@@ -0,0 +1,21 @@
+# dircolors
+eval "$(dircolors -b)"
+
+# load custom config
+. ~/.zsh/alias.zsh
+. ~/.zsh/directory.zsh
+. ~/.zsh/history.zsh
+. ~/.zsh/completion/completion.zsh
+
+# fzf
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# asdf-vm
+. $HOME/.asdf/asdf.sh
+. $HOME/.asdf/completions/asdf.bash
+
+# prompt
+autoload -U promptinit; promptinit
+prompt pure
+
+[ -f ~/.zsh/prompt/theme.zsh ] && . ~/.zsh/prompt/theme.zsh
