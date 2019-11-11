01291ba1c7a12a9b180eb8702b69760111773711
Florian Rand <florian.rand@gmail.com>
2019-11-11 14:12:19 +0100

    prompt

--- zsh/prompt/prompt.zsh
+++ zsh/prompt/prompt.zsh
@@ -4,4 +4,4 @@ GIT_PS1_SHOWUNTRACKEDFILES=true
 GIT_PS1_DESCRIBE_STYLE="branch"
 GIT_PS1_SHOWCOLORHINTS=true
 
-setopt PROMPT_SUBST ; precmd () { __git_ps1 "%n" ":%~$ " "|%s"  }
+setopt PROMPT_SUBST ; precmd () { __git_ps1 "%n" ":%2~$ " "|%s"  }
