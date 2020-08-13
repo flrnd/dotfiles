08ee96c61d2fc27a58b2b13638c4940ec6fba2fd
Florian Rand <florian.rand@gmail.com>
2020-08-13 17:22:37 +0200

    Using zplug

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,21 +1,33 @@
 # load custom config
 . ~/.zsh/alias.zsh
-. ~/.zsh/completion.zsh
-. ~/.zsh/directory.zsh
 . ~/.zsh/git/git.zsh
-. ~/.zsh/history.zsh
 . ~/.zsh/functions.zsh
-
-# fzf
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 # tilix
 if [ $TILIX_ID ] || [ $VTE_VERSION ]; then
   source /etc/profile.d/vte.sh
 fi
 
-# forgit
-. ~/.zsh/plugins/forgit/forgit.plugin.zsh
+## zplug
+. ~/.zplug/init.zsh
+
+zplug "modules/directory", from:prezto
+zplug "modules/completion", from:prezto
+zplug "modules/history", from:prezto
+zplug 'wfxr/forgit'
+
+# Install plugins if there are plugins that have not been installed
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    fi
+fi
+
+zplug load
+
+# fzf
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 # prompt
 . ~/.zsh/prompt/prompt.zsh
