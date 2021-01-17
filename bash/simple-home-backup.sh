efe421d6ca4cd552e1994e93503db4d4850187f4
Florian Rand <florian.rand@gmail.com>
2021-01-17 16:16:00 +0100

    Simple script to rsync some home files and dirs

---
+++ bash/simple-home-backup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+
+  case $key in
+    -d|--dest)
+      DEST_PATH="$2"
+      shift
+      shift
+      ;;
+    -f|--filelist)
+      FILE_LIST="$2"
+      FILEDIR_LIST=$(<$FILE_LIST)
+      shift
+      shift
+      ;;
+    *)
+      POSITIONAL+=("$1")
+      shift
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL[@]}"
+
+echo "DEST          = ${DEST_PATH}"
+echo "FILELIST      = ${FILE_LIST}"
+
+
+if [[ $DEST_PATH == $HOME ]] ; then
+  echo "Sync to $HOME"
+else
+  if [[ ! -d $DEST_PATH/$USER ]] ; then
+    echo "Creating Backup dest dir: $DEST_PATH/$USER"
+    mkdir -p $DEST_PATH/$USER
+  fi
+fi
+
+
+for dir in ${FILEDIR_LIST[@]}; do
+  rsync -avuP $HOME/$dir $DEST_PATH/$USER
+done
+echo "Backup sync done!"
