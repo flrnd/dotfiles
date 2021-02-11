d743111188cef518e1042f5638374ef9e9eee9d8
Florian Rand <florian.rand@gmail.com>
2021-02-11 19:05:13 +0100

    Add virt install command

---
+++ vm/installvm.sh
@@ -0,0 +1,11 @@
+sudo virt-install \
+            --name debian-kvm-img\
+            --memory 1024 \
+            --disk ~/.local/lib/libvirt/images/debian-10-generic-amd64.qcow2,device=disk,bus=virtio \
+            --disk ~/.local/lib/libvirt/images/generic.img,device=cdrom \
+            --os-type linux \
+            --os-variant debian10 \
+            --virt-type kvm \
+            --graphics none \
+            --network network=default,model=virtio \
+            --import
