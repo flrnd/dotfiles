3e6f0400d0835061f3fb1a1437db7cd305e6ceb2
Florian Rand <florian.rand@gmail.com>
2021-02-11 19:06:43 +0100

    s/generic/boot-generic/

--- vm/installvm.sh
+++ vm/installvm.sh
@@ -2,7 +2,7 @@ sudo virt-install \
             --name debian-kvm-img\
             --memory 1024 \
             --disk ~/.local/lib/libvirt/images/debian-10-generic-amd64.qcow2,device=disk,bus=virtio \
-            --disk ~/.local/lib/libvirt/images/generic.img,device=cdrom \
+            --disk ~/.local/lib/libvirt/images/boot-generic.img,device=cdrom \
             --os-type linux \
             --os-variant debian10 \
             --virt-type kvm \
