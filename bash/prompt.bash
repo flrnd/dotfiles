5f15abfae9d09451823703768fee033690a008a1
Florian Rand <florian.rand@gmail.com>
2021-07-25 19:05:05 +0200

    Use $ as prompt symbol

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -3,5 +3,5 @@ export GIT_PS1_SHOWCOLORHINTS=true
 export GIT_PS1_SHOWDIRTYSTATE=true
 export GIT_PS1_SHOWUNTRACKEDFILES=true
 export GIT_PS1_SHOWUPSTREAM="branch"
-export PROMPT_COMMAND='__git_ps1 "\[\033[01;34m\]\w\[\033[00m\]" "\n\[\033[01;35m\]>\[\033[00m\] "' # Git branch (relies on git-sh-prompt)
+export PROMPT_COMMAND='__git_ps1 "\[\033[01;34m\]\w\[\033[00m\]" "\n\[\033[01;35m\]$\[\033[00m\] "' # Git branch (relies on git-sh-prompt)
 
