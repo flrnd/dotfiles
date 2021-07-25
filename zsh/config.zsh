c248a5684199a27ddfe599a7bf055f2be6f3eca4
Florian Rand <florian.rand@gmail.com>
2021-07-25 18:57:48 +0200

    Add history and pluggins

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,26 +1,17 @@
-### Uncomment below for profiling
-# zmodload zsh/zprof
-
 # load custom config
 . ~/.zsh/alias.zsh
 . ~/.zsh/git/git.zsh
 . ~/.zsh/functions.zsh
+. ~/.zsh/history.zsh
 
 # tilix
 if [ $TILIX_ID ] || [ $VTE_VERSION ]; then
   source /etc/profile.d/vte.sh
 fi
 
-# zinit
-# . ~/.zsh/zinit/zinit.zsh
-
-# fzf
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+# Load pluggins
+. ~/.zsh/plugins/fzf.zsh
+. ~/.zsh/plugins/forgit.zsh
 
 # prompt
 . ~/.zsh/prompt/prompt.zsh
-
-## Uncomment below for profiling
-#zprof
-
-
