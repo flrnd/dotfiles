8f95ea8c2766ffde6b2e43657002aea0d13c85b9
Florian Rand <florian.rand@gmail.com>
2021-07-28 15:33:41 +0200

    Cleanup config

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,15 @@
-set -gx SHELL /usr/bin/fish
+switch (uname)
+    case Linux
+        set -gx SHELL /usr/bin/fish
+    case Darwin
+        set -gx SHELL /usr/local/bin/fish
+end
 set -lx EDITOR nvim
 set -lx VISUAL $EDITOR
 set -lx GIT_EDITOR $EDITOR
-set -lx PAGER "less"
+set -lx PAGER less
 set -lx FZF_DEFAULT_COMMAND "fd --type f --follow --hidden --exclude .git --exclude node_modules"
-set -lx BAT_THEME "OneHalfDark"
+# set -lx BAT_THEME "OneHalfDark"
 
 # Abbreviations
 abbr gcb "git checkout -b"
@@ -13,31 +18,29 @@ abbr gp "git push"
 
 # home bin
 if test -e ~/bin
-  set PATH ~/bin $PATH
+    set -gx PATH ~/bin $PATH
 end
 
 # local home bin
 if test -e ~/.local/bin
-  set PATH ~/.local/bin $PATH
+    set -gx PATH ~/.local/bin $PATH
 end
 
 # golang
 if test -e /usr/local/go/
-  set -lx GOPATH $HOME/go $GOPATH
-  set PATH $GOPATH/bin $PATH
+    set -gx GOPATH $HOME/go $GOPATH
+    set -gx PATH $GOPATH/bin $PATH
 end
 
 # Yarn
 if test -e ~/.yarn/bin
-  set PATH ~/.yarn/bin $PATH
+    set -gx PATH ~/.yarn/bin $PATH
+end
+
+# Rust
+if test -e ~/.cargo/bin
+    set -gx PATH ~/.cargo/bin $PATH
 end
 
 #prompt
 . ~/.config/fish/functions/prompt.fish
-
-#asdf-vm
-if test -e ~/.asdf
-  . ~/.asdf/asdf.fish
-end
-
-
