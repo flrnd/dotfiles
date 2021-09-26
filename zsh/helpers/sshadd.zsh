4d24793b9cd2204c6c9941054bc36930065eff21
Florian Rand <florian.rand@gmail.com>
2021-09-26 15:39:34 +0200

    Checks if ksshaskpass is installed

--- zsh/helpers/sshadd.zsh
+++ zsh/helpers/sshadd.zsh
@@ -1,4 +1,6 @@
-if [ "$DESKTOP_SESSION" = '/usr/share/xsessions/plasma5' ]; then
+if [ "$DESKTOP_SESSION" = '/usr/share/xsessions/plasma5' ] && type /usr/libexec/ssh/ksshaskpass &> /dev/null; then
 	export SSH_ASKPASS=/usr/libexec/ssh/ksshaskpass
 	/usr/bin/ssh-add -q </dev/null
+else
+	printf "%s\n" "Missing package ksshaskpass.\n"
 fi
