46da3d7e67956330e9c0b78eeb2d4f2fd1281891
Florian Rand <florian.rand@gmail.com>
2021-09-27 12:15:00 +0200

    Remove else

--- zsh/helpers/sshadd.zsh
+++ zsh/helpers/sshadd.zsh
@@ -1,6 +1,4 @@
 if [ "$DESKTOP_SESSION" = '/usr/share/xsessions/plasma5' ] && type /usr/libexec/ssh/ksshaskpass &> /dev/null; then
 	export SSH_ASKPASS=/usr/libexec/ssh/ksshaskpass
 	/usr/bin/ssh-add -q </dev/null
-else
-	printf "%s\n" "Missing package ksshaskpass.\n"
 fi
