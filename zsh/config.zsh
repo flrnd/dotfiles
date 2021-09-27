fe96b17639aa9d3feebdb996a8990905cc6c55d3
Florian Rand <florian.rand@gmail.com>
2021-09-27 13:25:05 +0200

    Revert "Remove tilix vte source"
    This reverts commit 0bcdbc36ac365b1102489c66fb4b9a790ef05e18.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -9,6 +9,11 @@
 # if Plasma use ksshask instead
 . ~/.zsh/helpers/sshadd.zsh
 
+# tilix
+if [ $TILIX_ID ] || [ $VTE_VERSION ]; then
+  source /etc/profile.d/vte.sh
+fi
+
 # Load pluggins
 . ~/.zsh/plugins/fzf.zsh
 . ~/.zsh/plugins/forgit.zsh
