500af4872b75c244d2f56f8e6afacd33524f180a
Florian Rand <florian.rand@gmail.com>
2021-12-06 13:23:07 +0100

    Delete file in favor of init.sh

--- bash/prompt.bash
+++
@@ -1,7 +0,0 @@
-source ~/.bash/git/init.sh
-export GIT_PS1_SHOWCOLORHINTS=true
-export GIT_PS1_SHOWDIRTYSTATE=true
-export GIT_PS1_SHOWUNTRACKEDFILES=true
-export GIT_PS1_SHOWUPSTREAM="branch"
-export PROMPT_COMMAND='__git_ps1 "\[\033[01;34m\]\w\[\033[00m\]" "\n\[\033[01;35m\]$\[\033[00m\] "' # Git branch (relies on git-sh-prompt)
-
