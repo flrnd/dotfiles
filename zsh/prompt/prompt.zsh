4447fd4c9b3154f6f4fdf5ddec92a17e83465f33
Florian Rand <florian.rand@gmail.com>
2021-12-07 18:29:58 +0100

    Fix prompt

--- zsh/prompt/prompt.zsh
+++ zsh/prompt/prompt.zsh
@@ -69,7 +69,5 @@ git_info() {
 # Use ❯ as the non-root prompt character; # for root
 # Change the prompt character color if the last command had a nonzero exit code
 #RPROMPT='%*'
-#PROMPT='$(ssh_info)%F{blue}%~%u%f $(git_info)
-#%(?.%F{magenta}.%F{red})%(!.#.$)%f%F{white}%f '
-
-export PROMPT_COMMAND='__git_ps1 "\w" "\n\\\$(ssh_info) $(git_info)"'
+PROMPT='$(ssh_info)%F{blue}%~%u%f $(git_info)
+%(?.%F{magenta}.%F{red})%(!.#.$)%f%F{white}%f '
