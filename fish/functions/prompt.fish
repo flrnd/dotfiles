8d3a0f37bb7ddadf373ee046c8fca791df18e71f
Florian Rand <florian.rand@gmail.com>
2021-12-07 15:08:09 +0100

    Informative

--- fish/functions/prompt.fish
+++ fish/functions/prompt.fish
@@ -10,7 +10,7 @@ set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showupstream 'yes'
-# set __fish_git_prompt_show_informative_status 'yes'
+set __fish_git_prompt_show_informative_status 'yes'
 set __fish_git_prompt_color_branch yellow
 set __fish_git_prompt_color_dirtystate red
 set __fish_git_prompt_color_invalidstate red
@@ -37,7 +37,7 @@ function fish_prompt
     set prompt_symbol_color red
   else
     set prompt_symbol '$'
-    set prompt_symbol_color magenta
+    set prompt_symbol_color green
   end
 
   set last_status $status
