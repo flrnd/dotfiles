ca94a7130ce9284922ad54cf89bd7fa3888f5283
Florian Rand <florian.rand@gmail.com>
2022-04-20 11:42:32 +0200

    Add color hints

--- bash/git/init.sh
+++ bash/git/init.sh
@@ -6,4 +6,5 @@ export GIT_PS1_SHOWCOLORHINTS=true
 export GIT_PS1_SHOWDIRTYSTATE=true
 export GIT_PS1_SHOWUNTRACKEDFILES=true
 export GIT_PS1_SHOWUPSTREAM="verbose"
+export GIT_PS1_SHOWCOLORHINTS=true
 export PROMPT_COMMAND='__git_ps1 "\w" "\n\\\$ "'
