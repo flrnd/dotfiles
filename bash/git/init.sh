0e8d3b2d86408fab464320da0a5e15bddeb9d060
Florian Rand <florian.rand@gmail.com>
2022-04-20 11:47:27 +0200

    Add upstream abbrev name

--- bash/git/init.sh
+++ bash/git/init.sh
@@ -5,6 +5,6 @@ fi
 export GIT_PS1_SHOWCOLORHINTS=true
 export GIT_PS1_SHOWDIRTYSTATE=true
 export GIT_PS1_SHOWUNTRACKEDFILES=true
-export GIT_PS1_SHOWUPSTREAM="verbose"
+export GIT_PS1_SHOWUPSTREAM="verbose name"
 export GIT_PS1_SHOWCOLORHINTS=true
 export PROMPT_COMMAND='__git_ps1 "\w" "\n\\\$ "'
