e91ad41481d36f28afa0dc93fd26e0ba2ff9e96b
Florian Rand <florian.rand@gmail.com>
2022-04-20 10:58:58 +0200

    Check if git-prompt.sh exist, change showpstream to verbose

--- bash/git/init.sh
+++ bash/git/init.sh
@@ -1,10 +1,9 @@
-if [ ! -f ~/.bash/git/git-prompt.sh  ]; then
-   curl -fsSLO https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+if [ -f /etc/bash_completion.d/git-prompt.sh ]; then
+	. /etc/bash_completion.d/git-prompt.sh
 fi
 
-. ~/.bash/git/git-prompt.sh
 export GIT_PS1_SHOWCOLORHINTS=true
 export GIT_PS1_SHOWDIRTYSTATE=true
 export GIT_PS1_SHOWUNTRACKEDFILES=true
-export GIT_PS1_SHOWUPSTREAM="branch"
+export GIT_PS1_SHOWUPSTREAM="verbose"
 export PROMPT_COMMAND='__git_ps1 "\w" "\n\\\$ "'
