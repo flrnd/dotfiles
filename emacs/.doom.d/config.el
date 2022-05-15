d94b7211f60468fa0420c625411628c20779a42d
Florian Rand <florian.rand@gmail.com>
2022-05-15 08:40:27 +0200

    Delete emacs old dotfiles.

--- emacs/.doom.d/config.el
+++
@@ -1,6 +0,0 @@
-;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here
-;; Global settings (defaults)
-(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-      doom-themes-enable-italic t) ; if nil, italics is universally disabled
