664d5a4bca7514750c795b7d607de3d9c4729ad6
Florian Rand <florian.rand@gmail.com>
2022-07-13 14:17:12 +0200

    Adds comment and key-bindings for fzf package

--- zsh/plugins/fzf.zsh
+++ zsh/plugins/fzf.zsh
@@ -1,4 +1,5 @@
 # fzf
-[ -f ~/.fzf.zsh ] && . ~/.fzf.zsh
+[ -f ~/.fzf.zsh ] && . ~/.fzf.zsh # manually installed from git
+[ -f /etc/zsh_completion.d/fzf-key-bindings ] && . /etc/zsh_completion.d/fzf-key-bindings
 
 
