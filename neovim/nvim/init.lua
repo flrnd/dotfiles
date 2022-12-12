6651cbe64d1a5eba7a352bd3608746dfaaba16f2
Florian Rand <florian.rand@gmail.com>
2022-12-12 16:41:01 +0100

    Add C-F grep

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -16,3 +16,7 @@ require('lualine').setup {
 vim.api.nvim_set_keymap('n', '<c-P>',
 	"<cmd>lua require('fzf-lua').files()<CR>",
 	{ noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<c-F>',
+	"<cmd>lua require('fzf-lua').grep()<CR>",
+	{ noremap = true, silent = true })
