587e6b1b06df7534de98c7361d58deb0b5b08d6e
Florian Rand <florian.rand@gmail.com>
2022-12-14 22:03:40 +0100

    Add fzf package bindings

--- zsh/plugins/fzf.zsh
+++ zsh/plugins/fzf.zsh
@@ -1,5 +1,4 @@
 # fzf
 [ -f ~/.fzf.zsh ] && . ~/.fzf.zsh # manually installed from git
 [ -f /etc/zsh_completion.d/fzf-key-bindings ] && . /etc/zsh_completion.d/fzf-key-bindings
-
-
+[ -f /usr/share/fzf/key-bindings.zsh ] && . /usr/share/fzf/key-bindings.zsh # installed from package
