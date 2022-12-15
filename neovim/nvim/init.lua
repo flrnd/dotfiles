86a5bc2f543bac3804eecf5febc9d9b0391324e7
Florian Rand <florian.rand@gmail.com>
2022-12-15 19:49:53 +0100

    clean the old config

--- neovim/nvim/init.lua
+++
@@ -1,7 +0,0 @@
-require "settings"
-require("plugins").setup()
-
-vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
-
--- vim.g.tokyonight_style = "day"
-vim.cmd [[colorscheme tokyonight]]
