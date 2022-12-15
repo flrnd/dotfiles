fd955bf3dd4ed877500c00eaf479f2b9f6cbb843
Florian Rand <florian.rand@gmail.com>
2022-12-15 19:53:14 +0100

    Add new config with LSP working

---
+++ nvim/init.lua
@@ -0,0 +1,137 @@
+local cmd = vim.cmd
+local fn = vim.fn
+local api = vim.api
+
+local packer_bootstrap = false -- Indicate first time installation
+
+-- packer.nvim configuration
+local conf = {
+  profile = {
+    enable = true,
+    threshold = 0, -- the amount in ms that a plugins load time must be over for it to be included in the profile
+  },
+
+  display = {
+    open_fn = function()
+      return require("packer.util").float({ border = "rounded" })
+    end,
+  },
+}
+
+local function packer_init()
+  -- Check if packer.nvim is installed
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    packer_bootstrap = fn.system({
+      "git",
+      "clone",
+      "--depth",
+      "1",
+      "https://github.com/wbthomason/packer.nvim",
+      install_path,
+    })
+    cmd([[packadd packer.nvim]])
+  end
+
+  -- Run PackerCompile if there are changes in this file
+  local packerGrp = api.nvim_create_augroup("packer_user_config", { clear = true })
+  api.nvim_create_autocmd(
+    { "BufWritePost" },
+    { pattern = "init.lua", command = "source <afile> | PackerCompile", group = packerGrp }
+  )
+end
+
+-- Plugins
+local function plugins(use)
+  use({ "wbthomason/packer.nvim" })
+  use({ "kyazdani42/nvim-web-devicons" })
+  use({ "nvim-lua/plenary.nvim" })
+  use { 'junegunn/fzf' }
+  use { 'ibhagwan/fzf-lua',
+    -- optional for icon support
+    requires = { 'nvim-tree/nvim-web-devicons' }
+  }
+  use {
+    "gfanto/fzf-lsp.nvim",
+    config = function()
+      require('fzf_lsp').setup()
+    end,
+  }
+  use {
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+      require("nvim-surround").setup()
+    end,
+  }
+  -- Auto pairs
+  use {
+    "windwp/nvim-autopairs",
+    opt = true,
+    event = "InsertEnter",
+    module = { "nvim-autopairs.completion.cmp", "nvim-autopairs" },
+    config = function()
+      require("config.autopairs").setup()
+    end,
+  }
+  use({
+    "neovim/nvim-lspconfig",
+    event = "BufReadPre",
+    requires = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      {
+        "L3MON4D3/LuaSnip",
+      },
+    },
+    config = function()
+      require("config.lsp")
+    end,
+  })
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+    config = function()
+      require("config.treesitter")
+    end,
+  })
+  use({ "nvim-treesitter/playground" })
+  -- colorscheme
+  use "folke/tokyonight.nvim"
+  -- lualine
+  use {
+    "nvim-lualine/lualine.nvim",
+    requires = { "kyazdani42/nvim-web-devicons", opt = true },
+    config = function()
+      require("lualine").setup {
+        options = {
+          theme = "tokyonight"
+        }
+      }
+    end,
+  }
+  -- WhechKey
+  use {
+    "folke/which-key.nvim",
+    event = "VimEnter",
+    module = { "which-key" },
+    -- keys = { [[<leader>]] },
+    config = function()
+      require("config.whichkey").setup()
+    end,
+  }
+
+  -- Bootstrap Neovim
+  if packer_bootstrap then
+    print("Neovim restart is required after installation!")
+    require("packer").sync()
+  end
+end
+
+-- packer.nvim
+packer_init()
+local packer = require("packer")
+packer.init(conf)
+packer.startup(plugins)
+
+cmd [[colorscheme tokyonight]]
---
+++ nvim/lua/config/whichkey.lua
@@ -0,0 +1,74 @@
+local M = {}
+
+function M.setup()
+	local whichkey = require "which-key"
+
+	local conf = {
+		window = {
+			border = "single", -- none, single, double, shadow
+			position = "bottom", -- bottom, top
+		},
+	}
+
+	local opts = {
+		mode = "n", -- Normal mode
+		prefix = "<leader>",
+		buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+		silent = true, -- use `silent` when creating keymaps
+		noremap = true, -- use `noremap` when creating keymaps
+		nowait = false, -- use `nowait` when creating keymaps
+	}
+
+	local mappings = {
+		["w"] = { "<cmd>update!<CR>", "Save" },
+		["q"] = { "<cmd>q!<CR>", "Quit" },
+
+		b = {
+			name = "Buffer",
+			c = { "<Cmd>bd!<Cr>", "Close current buffer" },
+			D = { "<Cmd>%bd|e#|bd#<Cr>", "Delete all buffers" },
+		},
+
+		f = {
+			name = "Find",
+			f = { "<cmd>lua require('utils.finder').find_files()<cr>", "Files" },
+			b = { "<cmd>FzfLua buffers<cr>", "Buffers" },
+			o = { "<cmd>FzfLua oldfiles<cr>", "Old files" },
+			g = { "<cmd>FzfLua live_grep<cr>", "Live grep" },
+			c = { "<cmd>FzfLua commands<cr>", "Commands" },
+		},
+
+		g = {
+			name = "Goto",
+			d = { "<Cmd>lua vim.lsp.buf.definition()<CR>", "Definition" },
+			D = { "<Cmd>lua vim.lsp.buf.declaration()<CR>", "Declaration" },
+			s = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature Help" },
+			I = { "<cmd>lua vim.lsp.buf.implementation()<CR>", "Goto Implementation" },
+			t = { "<cmd>lua vim.lsp.buf.type_definition()<CR>", "Goto Type Definition" },
+		},
+
+		l = {
+			name = "Code",
+			r = { "<cmd>lua vim.lsp.buf.rename()<CR>", "Rename" },
+			a = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "Code Action" },
+			d = { "<cmd>lua vim.diagnostic.open_float()<CR>", "Line Diagnostics" },
+			i = { "<cmd>LspInfo<CR>", "Lsp Info" },
+		},
+
+		p = {
+			name = "Packer",
+			c = { "<cmd>PackerCompile<cr>", "Compile" },
+			i = { "<cmd>PackerInstall<cr>", "Install" },
+			p = { "<cmd>PackerProfile<cr>", "Profile" },
+			s = { "<cmd>PackerSync<cr>", "Sync" },
+			S = { "<cmd>PackerStatus<cr>", "Status" },
+			u = { "<cmd>PackerUpdate<cr>", "Update" },
+		},
+
+	}
+
+	whichkey.setup(conf)
+	whichkey.register(mappings, opts)
+end
+
+return M
