c3f44a34918b7b5d2ee259ee32862898a8274b77
Florian Rand <florian.rand@gmail.com>
2022-12-16 17:14:35 +0100

    Add route for fedora

--- zsh/plugins/fzf.zsh
+++ zsh/plugins/fzf.zsh
@@ -1,4 +1,6 @@
 # fzf
 [ -f ~/.fzf.zsh ] && . ~/.fzf.zsh # manually installed from git
 [ -f /etc/zsh_completion.d/fzf-key-bindings ] && . /etc/zsh_completion.d/fzf-key-bindings
-[ -f /usr/share/fzf/key-bindings.zsh ] && . /usr/share/fzf/key-bindings.zsh # installed from package
+
+[ -f /usr/share/fzf/key-bindings.zsh ] && . /usr/share/fzf/key-bindings.zsh # installed from package (openSUSE)
+[ -f /usr/share/fzf/shell/key-bindings.zsh ] && . /usr/share/fzf/shell/key-bindings.zsh # installed from package (Fedora)
