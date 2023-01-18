049ab062b5838d30942f79cc87675174b7ff07aa
Florian Rand <florian.rand@gmail.com>
2023-01-18 16:36:56 +0100

    Add ssh agent and load fzf function for tab completion

--- zsh/plugins/fzf.zsh
+++ zsh/plugins/fzf.zsh
@@ -2,5 +2,9 @@
 [ -f ~/.fzf.zsh ] && . ~/.fzf.zsh # manually installed from git
 [ -f /etc/zsh_completion.d/fzf-key-bindings ] && . /etc/zsh_completion.d/fzf-key-bindings
 
-[ -f /usr/share/fzf/key-bindings.zsh ] && . /usr/share/fzf/key-bindings.zsh # installed from package (openSUSE)
-[ -f /usr/share/fzf/shell/key-bindings.zsh ] && . /usr/share/fzf/shell/key-bindings.zsh # installed from package (Fedora)
+# openSUSE
+[ -f /usr/share/fzf/key-bindings.zsh ] && . /usr/share/fzf/key-bindings.zsh
+[ -f /usr/share/zsh/site-functions/_fzf ] && . /usr/share/zsh/site-functions/_fzf
+
+#Fedora
+[ -f /usr/share/fzf/shell/key-bindings.zsh ] && . /usr/share/fzf/shell/key-bindings.zsh
