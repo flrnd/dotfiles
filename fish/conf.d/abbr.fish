fcf62e8eb12b1be3f6ef021058c07ac1896a7911
Florian Rand <florian.rand@gmail.com>
2023-01-31 16:49:59 +0100

    Add some abbr for zypper

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -2,16 +2,28 @@
 abbr -a nv "nvim"
 abbr -a vi "nvim"
 abbr -a svi "sudo nvim"
+
 abbr -a gc "git commit --verbose"
 abbr -a gcb "git checkout -b"
 abbr -a gp "git push"
+
 abbr -a cp "cp -i"
 abbr -a mv "mv -i"
 abbr -a rm "rm -i"
-abbr -a rmf "rm -ivrf"
+abbr -a rmr "rm -ir"
+abbr -a rmrf "rm -irf"
 abbr -a mkdir "mkdir -p"
+
 abbr -a 7zx "7z x"
 abbr -a 7zc "7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on"
+
 abbr -a tar "tar cv"
 abbr -a untar "tar xv"
+
 abbr -a lt "du -sh | sort -h"
+
+abbr -a zs "zypper se"
+abbr -a szr "sudo zypper ref"
+abbr -a szi "sudo zypper in"
+abbr -a szr "sudo zypper remove --clean-deps"
+abbr -a szd "sudo zypper dup --no-recommends"
