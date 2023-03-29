7f8a8b0799f8f05ccc854cfe4f4a450a01beea4e
Florian Rand <florian.rand@gmail.com>
2023-03-29 11:54:17 +0200

    Add local go install check

--- fish/config.fish
+++ fish/config.fish
@@ -18,6 +18,10 @@ if test -e "$HOME/.local/bin"
 end
 
 # go
+if test -e "$HOME/.local/go"
+  set -gx PATH "$HOME/.local/go/bin" $PATH
+end
+
 if test -e "$HOME/go"
   set -gx PATH "$HOME/go/bin" $PATH
   set -gx GOPATH "$HOME/go"
