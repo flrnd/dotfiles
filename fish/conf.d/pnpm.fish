c8d5625b714cc001e5340ccf3f3a7ff7d6533f11
Florian Rand <florian.rand@gmail.com>
2023-04-17 09:33:02 +0200

    Add pnpm conf env

--- fish/conf.d/pnpm.fish
+++ fish/conf.d/pnpm.fish
@@ -1,5 +1,7 @@
 if test -d "$HOME/.local/share/pnpm"
   set -gx PNPM_HOME "/home/mrfb/.local/share/pnpm"
-  set -gx PATH "$PNPM_HOME" $PATH
+    if not string match -q -- $PNPM_HOME $PATH
+      set -gx PATH "$PNPM_HOME" $PATH
+    end
 end
 
