f0e290f983bb04a077423a7f3c40df8a45e4bebf
Florian Rand <florian.rand@gmail.com>
2023-06-21 13:27:15 +0200

    update fzf config

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -1,5 +1,7 @@
 set -gx FZF_DEFAULT_COMMAND "fd --type f --follow --hidden --exclude .git --exclude node_modules"
 
-if test -e "/usr/share/fish/vendor_functions.d/fzf_key_bindings.fish"
+if test -e "$HOME/.fzf/shell/key-bindings.fish"
+  source "$HOME/.fzf/shell/key-bindings.fish"
+else if test -e "/usr/share/fish/vendor_functions.d/fzf_key_bindings.fish"
   source "/usr/share/fish/vendor_functions.d/fzf_key_bindings.fish"
 end
