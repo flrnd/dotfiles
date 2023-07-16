188bb94a7b2989dc18e3717cf0133d7ef6329ec7
Florian Rand <florian.rand@gmail.com>
2023-07-16 11:02:25 +0200

    Suse no more. Update abbr

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -2,13 +2,14 @@
 abbr -a nv "nvim"
 abbr -a vi "nvim"
 abbr -a svi "sudo nvim"
+abbr -a sedit "sudoedit"
 
 abbr -a gc "git commit --verbose"
 abbr -a gcb "git checkout -b"
 abbr -a gp "git push"
 
-abbr -a cp "cp -i"
-abbr -a mv "mv -i"
+abbr -a cpi "cp -i"
+abbr -a mvi "mv -i"
 abbr -a rmi "rm -i"
 abbr -a rmr "rm -ir"
 abbr -a rmrf "rm -rf"
@@ -17,13 +18,7 @@ abbr -a mkdir "mkdir -p"
 abbr -a 7zx "7z x"
 abbr -a 7zc "7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on"
 
-abbr -a tar "tar cv"
-abbr -a untar "tar xv"
+abbr -a tar "tar cvf"
+abbr -a untar "tar xvzf"
 
 abbr -a lt "du -sh | sort -h"
-
-abbr -a zs "zypper se"
-abbr -a szr "sudo zypper ref"
-abbr -a szi "sudo zypper in"
-abbr -a szr "sudo zypper remove --clean-deps"
-abbr -a szd "sudo zypper dup --no-recommends"
