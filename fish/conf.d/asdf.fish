ad520efe575e7799f4d25d75c1ed863d2349e357
Florian Rand <florian.rand@gmail.com>
2023-11-13 16:53:19 +0100

    Add asdf config with golang plug check

---
+++ fish/conf.d/asdf.fish
@@ -0,0 +1,10 @@
+if test -d "$HOME/.asdf"
+  source "$HOME/.asdf/asdf.fish"
+  if ! test -L "$HOME/.config/fish/completions/asdf.fish"
+    ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+  end
+
+  if test -d "$HOME/.asdf/plugins/golang/set-env.fish"
+    source "$HOME/.asdf/plugins/golang/set-env.fish"
+  end
+end
