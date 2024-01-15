1e473741acc0d407a131fc7e9be62148137546fc
Florian Rand <florian.rand@gmail.com>
2024-01-15 10:47:27 +0100

    link codespaces bin to home

--- setup.sh
+++ setup.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-ln -s bash/scripts ~/bin
-ln -s zsh/alias.zsh ~/.zsh_aliases
-ln -s zsh/config.zsh ~/.zshrc
-ln -s zsh/env.zsh ~/.zshenv
+ln -s /workspaces/.codespaces/.persistedshare/dotfiles/bash/scripts ~/bin
