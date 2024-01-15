80c47da47d7b7fd8bc35b284ac9470e2e6d7917f
Florian Rand <florian.rand@gmail.com>
2024-01-15 12:11:14 +0100

    update setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-ln -s /workspaces/.codespaces/.persistedshare/dotfiles/bash/bin ~/bin
+mkdir -p ~/bin
+ln -s /workspaces/.codespaces/.persistedshare/dotfiles/bash/bin/git-new ~/bin/git-new
 ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.bashrc ~/.bashrc
-ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.profile ~/.profile
+ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.profile ~/.bash_profile
 ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.bash_aliases ~/.bash_aliases
 ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.gitconfig ~/.gitconfig
