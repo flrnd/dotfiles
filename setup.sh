a4a535d1cb10323507027bd3e3a00d7e5e55727e
Florian Rand <florian.rand@gmail.com>
2024-01-24 12:12:55 +0100

    Add git prompt to setup

--- setup.sh
+++ setup.sh
@@ -14,4 +14,4 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 echo "export FZF_BASE=~/.fzf" >> ~/.zshrc
 echo 'export PATH="$HOME/bin:$PATH"' >> ~/.zshrc
 
-sed -i 's/plugins=(git)/plugins=(git forgit fzf)/' ~/.zshrc
+sed -i 's/plugins=(git)/plugins=(git git-prompt forgit fzf)/' ~/.zshrc
