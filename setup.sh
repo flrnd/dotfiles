2af7c595657b1ff642f2d4727364628eafea9b5e
Florian Rand <florian.rand@gmail.com>
2024-02-19 12:29:19 +0100

    Update setup.sh with neovim, ripgrep and fd-find

--- setup.sh
+++ setup.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-sudo apt-get -y update && sudo apt-get -y install fish
+sudo apt-get -y update && sudo apt-get -y install fish ripgrep fd-find neovim
 
 mkdir -p ~/bin
 ln -s /workspaces/.codespaces/.persistedshare/dotfiles/bash/bin/git-new ~/bin/git-new
 ln -s /workspaces/.codespaces/.persistedshare/dotfiles/fish ~/.config/
 
 # forgit
-#git clone https://github.com/wfxr/forgit.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/forgit
 git clone https://github.com/wfxr/forgit.git ~/.forgit
+
 # fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
-
-#echo "export FZF_BASE=~/.fzf" >> ~/.zshrc
-#echo 'export PATH="$HOME/bin:$PATH"' >> ~/.zshrc
-
-#sed -i 's/plugins=(git)/plugins=(git git-prompt forgit fzf)/' ~/.zshrc
