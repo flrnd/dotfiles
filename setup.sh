72e660b0960a3c960c3754670f8ff1cf0673cd96
Florian Rand <florian.rand@gmail.com>
2024-02-26 14:34:54 +0100

    Add bat

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo apt-get -y update && sudo apt-get -y install fish ripgrep fd-find neovim
+sudo apt-get -y update && sudo apt-get -y install bat fish fd-find neovim ripgrep
 
 mkdir -p ~/bin
+
 ln -s /workspaces/.codespaces/.persistedshare/dotfiles/bash/bin/git-new ~/bin/git-new
 ln -s /workspaces/.codespaces/.persistedshare/dotfiles/fish ~/.config/
 
