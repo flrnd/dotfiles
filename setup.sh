d7c945013960f29681092e621afdd53ef8547d01
Florian Rand <florian.rand@gmail.com>
2024-02-29 10:37:38 +0100

    Use new js git-new version

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt-get -y update && sudo apt-get -y install bat fish fd-find hub neovim ri
 
 mkdir -p ~/bin
 
-ln -s /workspaces/.codespaces/.persistedshare/dotfiles/bash/bin/git-new ~/bin/git-new
+ln -s /workspaces/.codespaces/.persistedshare/dotfiles/git-new/git-new ~/bin/git-new
 ln -s /workspaces/.codespaces/.persistedshare/dotfiles/fish ~/.config/
 
 # forgit
