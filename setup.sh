9c82b3ce27a9929369d0c1ac5683bbbd7266b503
Florian Rand <florian.rand@gmail.com>
2024-02-29 10:52:53 +0100

    Copy instead of ln

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ sudo apt-get -y update && sudo apt-get -y install bat fish fd-find hub neovim ri
 
 mkdir -p ~/bin
 
-ln -s /workspaces/.codespaces/.persistedshare/dotfiles/git-new/git-new ~/bin/git-new
-ln -s /workspaces/.codespaces/.persistedshare/dotfiles/fish ~/.config/
+cp /workspaces/.codespaces/.persistedshare/dotfiles/git-new/git-new ~/bin/git-new
+cp -r /workspaces/.codespaces/.persistedshare/dotfiles/fish ~/.config/
 
 # forgit
 git clone https://github.com/wfxr/forgit.git ~/.forgit
