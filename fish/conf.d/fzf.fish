0931f4b1c07d1a226e77ea5b6129969519f951c2
flrnd <florian.rand@gmail.com>
2024-04-29 22:26:54 +0200

    Add fzf keybindings for fedora

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -1,7 +1,9 @@
 set -gx FZF_DEFAULT_COMMAND "fd --type f --follow --hidden --exclude .git --exclude node_modules"
 
 if test -e "$HOME/.fzf/shell/key-bindings.fish"
-  source "$HOME/.fzf/shell/key-bindings.fish"
-else if test -e "/usr/share/fish/vendor_functions.d/fzf_key_bindings.fish"
-  source "/usr/share/fish/vendor_functions.d/fzf_key_bindings.fish"
+  source "$HOME/.fzf/shell/key-bindings.fish"i
+end
+
+if test -e "/usr/share/fzf/shell/key-bindings.fish"
+  source "/usr/share/fzf/shell/key-bindings.fish"
 end
