daf2d786c52f42b21d1750b29bd082e1bdfaaeeb
flrnd <florian.rand@gmail.com>
2024-05-14 15:56:59 +0200

    Add force color var

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@ set -g fish_greeting ""
 set -gx EDITOR (which nvim)
 set -gx VISUAL $EDITOR
 set -gx GIT_EDITOR $EDITOR
-
+set -gx FORCE_COLOR true
 set -g MANPAGER "sh -c 'col -bx | bat -l man -p'"
 set -g MANROFFOPT "-c"
 
