b0b166ecabaae51ae662048c2b74b5396f3c6b76
flrnd <florian.rand@gmail.com>
2024-07-08 00:28:58 +0200

    Fix wrong packer boostrap statement

--- nvim/init.lua
+++ nvim/init.lua
@@ -195,8 +195,8 @@ require("packer").startup(function(use)
     plugins(use)
   end
 
-  if is_bootstrap then
-    require("packer").sync()
+  if packer_bootstrap then
+    require('packer').sync()
   end
 end)
 
@@ -204,7 +204,7 @@ end)
 -- make sense to execute the rest of the init.lua.
 --
 -- You'll need to restart nvim, and then it will work.
-if is_bootstrap then
+if packer_bootstrap then
   print("==================================")
   print("    Plugins are being installed")
   print("    Wait until Packer completes,")
