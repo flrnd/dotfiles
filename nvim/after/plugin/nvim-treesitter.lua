5f0cb75aabf7ec83710dfa9497f6fdae2322af60
flrnd <florian.rand@gmail.com>
2024-07-30 12:31:08 +0200

    Rename to nvim-treesitter

--- nvim/after/plugin/nvim-treesitter.lua
+++ nvim/after/plugin/nvim-treesitter.lua
@@ -2,7 +2,29 @@
 -- See `:help nvim-treesitter`
 require("nvim-treesitter.configs").setup({
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { "c", "go", "lua", "rust", "typescript", "graphql", "css", "fish", "html", "vimdoc", "markdown" },
+  build = ":TSUpdate",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  ensure_installed = {
+    "bash",
+    "c",
+    "go",
+    "lua",
+    "rust",
+    "html",
+    "json",
+    "javascript",
+    "typescript",
+    "graphql",
+    "css",
+    "fish",
+    "html",
+    "vimdoc",
+    "yaml",
+    "dockerfile",
+    "markdown"
+  },
 
   highlight = { enable = true },
   indent = { enable = true },
