93ba010ea23baff6a98cbf8314e5eadc10fdf13c
flrnd <florian.rand@gmail.com>
2025-03-31 14:22:47 +0000

    Feat: add nixfmt and exec fish as main shell

--- nix/shell.nix
+++ nix/shell.nix
@@ -5,13 +5,19 @@ pkgs.mkShell {
     pkgs.bat
     pkgs.curl
     pkgs.fd
+    pkgs.fish
     pkgs.fzf
     pkgs.git
     pkgs.httpie
     pkgs.jless
     pkgs.jq
     pkgs.neovim
+    pkgs.nixfmt-rfc-style
     pkgs.ripgrep
     pkgs.tig
- ];
+  ];
+
+  shellHook = ''
+    exec fish
+  '';
 }
