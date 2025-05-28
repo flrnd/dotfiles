41a1b5bbb6ebb05e0d18827c89c29cf525d2c38c
flrnd <florian.rand@gmail.com>
2025-05-28 12:18:30 +0200

    Update forgit path to .local/share

--- fish/conf.d/forgit.plugin.fish
+++ fish/conf.d/forgit.plugin.fish
@@ -1,3 +1,3 @@
-if test -e "$HOME/.forgit/conf.d/forgit.plugin.fish"
-  source "$HOME/.forgit/conf.d/forgit.plugin.fish"
+if test -e "$HOME/.local/share/forgit/conf.d/forgit.plugin.fish"
+  source "$HOME/.local/share/forgit/conf.d/forgit.plugin.fish"
 end
