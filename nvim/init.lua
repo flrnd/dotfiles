732203899e007e2249e7935f48afd73490b966c4
flrnd <raul.perez@florianrand.com>
2025-07-06 12:20:35 +0200

    Removed unused. Refactor deprecated highlight

--- nvim/init.lua
+++ nvim/init.lua
@@ -134,7 +134,6 @@ require("packer").startup(function(use)
       "SmiteshP/nvim-navic",
       "nvim-tree/nvim-web-devicons", -- optional dependency
     },
-    after = "nvim-web-devicons",     -- keep this if you're using NvChad
     config = function()
       require("barbecue").setup()
     end,
