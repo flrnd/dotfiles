04708afbd2dc0f1f84a7cbd9e1742f98257b06f8
flrnd <659650+flrnd@users.noreply.github.com>
2025-07-30 17:55:00 +0200

    Add linuxbrew support

--- fish/conf.d/brew.fish
+++ fish/conf.d/brew.fish
@@ -1,3 +1,7 @@
-if test -e "/opt/homebrew/bin/brew"
+if test -e /opt/homebrew/bin/brew
     eval "$(/opt/homebrew/bin/brew shellenv)"
+end
+
+if test -e /home/linuxbrew
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 end
