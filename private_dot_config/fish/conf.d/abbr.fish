6721510c00393a97aec68bb1cb07434e036e492f
flrnd <659650+flrnd@users.noreply.github.com>
2025-08-08 16:12:59 +0200

    Clean abbr definitions

--- private_dot_config/fish/conf.d/abbr.fish
+++ private_dot_config/fish/conf.d/abbr.fish
@@ -1,19 +1,11 @@
 # Abbr
-abbr -a nv "nvim"
-abbr -a vi "nvim"
+abbr -a vi nvim
 abbr -a svi "sudo nvim"
-abbr -a sedit "sudoedit"
+abbr -a sedit sudoedit
 
 abbr -a gc "git commit --verbose"
-abbr -a gcs "git commit -sS --verbose"
 abbr -a gcb "git checkout -b"
-abbr -a gp "git push"
 
-abbr -a cpi "cp -i"
-abbr -a mvi "mv -i"
-abbr -a rmi "rm -i"
-abbr -a rmr "rm -ir"
-abbr -a rmrf "rm -rf"
 abbr -a mkdir "mkdir -p"
 
 abbr -a 7zx "7z x"
